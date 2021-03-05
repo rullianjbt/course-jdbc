0af4db68834ef5395131725b3985169cf30a4438
Rullian Siqueira da Mata <rullianjbt@gmail.com>
2021-03-05 12:04:11 -0300

    Example using jdbc to select data from database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,9 +11,29 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection conn = DB.getConnection(); 
-		DB.closeConnection();
-
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", "+rs.getString("name"));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();	
+		}
 	}
 
 }
